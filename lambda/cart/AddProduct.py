754cace3f6b0d28a1fdb5a8d8ef672dadb6876ec
nay <nayeli.guzman@utec.edu.pe>
2025-06-15 00:51:27 -0500

    add: lambdas M3

--- lambda/cart/AddProduct.py
+++ lambda/cart/AddProduct.py
@@ -16,13 +16,13 @@ def lambda_handler(event, context):
     tenant_id = body['tenant_id']
 
     lambda_client = boto3.client('lambda')    
-    payload_string =  json.dumps({
-        "token": token,
-        "tenant_id": tenant_id 
-    })
+    payload = {
+    "token": token,
+    "tenant_id": tenant_id
+    }
     invoke_response = lambda_client.invoke(FunctionName=user_validar,
                                            InvocationType='RequestResponse',
-                                           Payload = payload_string)
+                                           Payload = json.dumps(payload))
     response = json.loads(invoke_response['Payload'].read())
     print(response)
     if response['statusCode'] == 403:
