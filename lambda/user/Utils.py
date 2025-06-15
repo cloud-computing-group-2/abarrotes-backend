a1ccc2833a78b409d14676310c2be348b334ba2a
nay <nayeli.guzman@utec.edu.pe>
2025-06-15 00:45:25 -0500

    add: lambdas M3

--- lambda/user/Utils.py
+++ lambda/user/Utils.py
@@ -1,7 +1,14 @@
 import json
 
+import json
+
 def load_body(event):
+    if 'body' not in event:
+        return event
+    
     if isinstance(event["body"], dict):
         return event['body']
     else:
         return json.loads(event['body'])
+
+
