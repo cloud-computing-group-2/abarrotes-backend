77aadeffaf217c62eb6d9312c972667503d8cc83
salva <atlas200x@gmail.com>
2025-07-05 17:03:43 -0500

    fix: swagger productos

--- lambda/product/swagger.js
+++ lambda/product/swagger.js
@@ -1,26 +1,28 @@
-const html = `
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Abarrotes Productos API Docs</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
-</head>
-<body>
-  <div id="swagger-ui"></div>
-  <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
-  <script>
-    window.onload = () => {
-      SwaggerUIBundle({
-        url: '/dev/openapi.yaml',  // asegúrate que coincide con tu stage
-        dom_id: '#swagger-ui',
-      });
-    };
-  </script>
-</body>
-</html>
-`;
+const fs = require('fs');
+const path = require('path');
 
 module.exports.lambda_handler = async () => {
+  const html = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Abarrotes Productos API Docs</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+  </head>
+  <body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+    <script>
+      window.onload = () => {
+        SwaggerUIBundle({
+          url: '/dev/openapi.yaml',
+          dom_id: '#swagger-ui',
+        });
+      };
+    </script>
+  </body>
+  </html>
+  `;
   return {
     statusCode: 200,
     headers: {
@@ -32,19 +34,16 @@ module.exports.lambda_handler = async () => {
 };
 
 module.exports.openapi_handler = async () => {
-  const fs = require('fs');
-  const path = require('path');
-  const filePath = path.join(__dirname, 'openapi.yaml');
-
+  const filePath = path.join(__dirname, '../openapi.yaml');
   try {
-    const openapi = fs.readFileSync(filePath, 'utf8');
+    const content = fs.readFileSync(filePath, 'utf8');
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/yaml',
         'Access-Control-Allow-Origin': '*',
       },
-      body: openapi,
+      body: content,
     };
   } catch (err) {
     return {
